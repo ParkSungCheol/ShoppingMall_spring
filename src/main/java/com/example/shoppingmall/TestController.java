f212a423998301539da8f8e6248d24e84f42280d
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-15 16:41:30 +0900

    second build

--- src/main/java/com/example/shoppingmall/TestController.java
+++ src/main/java/com/example/shoppingmall/TestController.java
@@ -1,23 +1,15 @@
 package com.example.shoppingmall;
 
-import com.example.shoppingmall.Domain.User;
-import com.example.shoppingmall.Service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
-    private final UserService userService;
-
-    @GetMapping
-    public List<User> getUserList () {
-        return userService.getUserList();
+    @GetMapping("/data")
+    public String test() {
+        return "first build";
     }
 }
