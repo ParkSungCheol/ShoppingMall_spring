f50e91c69e7408b360e23db736f3580d4b4c5dc0
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-17 15:16:50 +0900

    db connect

--- src/main/java/com/example/shoppingmall/TestController.java
+++ src/main/java/com/example/shoppingmall/TestController.java
@@ -1,14 +1,23 @@
 package com.example.shoppingmall;
 
+import com.example.shoppingmall.Domain.User;
+import com.example.shoppingmall.Service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String test() {
-        return "test";
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getUserList () {
+        return userService.getUserList();
     }
 }
