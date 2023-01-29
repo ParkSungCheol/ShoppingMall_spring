ac55339bbe0e7761789e3f5d4fcbc24379c83722
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-29 20:57:16 +0900

    test

--- src/main/java/com/example/shoppingmall/TestController.java
+++ src/main/java/com/example/shoppingmall/TestController.java
@@ -3,6 +3,7 @@ package com.example.shoppingmall;
 import com.example.shoppingmall.Domain.User;
 import com.example.shoppingmall.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin("http://ec2-43-201-71-180.ap-northeast-2.compute.amazonaws.com")
 public class TestController {
 
     private final UserService userService;
