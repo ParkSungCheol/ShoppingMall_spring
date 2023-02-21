33c70074243fa7c1c06b9a078b9d979042795581
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-21 16:20:56 +0900

    test

--- src/main/java/com/example/shoppingmall/TestController.java
+++ src/main/java/com/example/shoppingmall/TestController.java
@@ -42,7 +42,7 @@ public class TestController {
     @GetMapping("/")
     public String getTest () {
     	System.out.println("test");
-        return "2023-02-21";
+        return "2023-02-22";
     }
 
     @GetMapping("/user")
