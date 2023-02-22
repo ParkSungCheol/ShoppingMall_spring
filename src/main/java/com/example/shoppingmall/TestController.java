ca46e381a4ab32fb4031d07e8453f81450312c3b
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-22 15:37:12 +0900

    cookie test

--- src/main/java/com/example/shoppingmall/TestController.java
+++ src/main/java/com/example/shoppingmall/TestController.java
@@ -64,8 +64,8 @@ public class TestController {
     	boolean isVaild = userService.loginCheck(user, session);
     	
     	if(isVaild) {
-    		Cookie myCookie = new Cookie("value1", "www.juromarket.kro.kr");
-    		myCookie.setDomain("www.juromarket.kro.kr");
+    		Cookie myCookie = new Cookie("value1", "www.juromarket.kro.kr/#/");
+    		myCookie.setDomain("www.juromarket.kro.kr/#/");
     		response.addCookie(myCookie);
     		return new ResponseEntity<>(null, HttpStatus.OK); 
     	}
