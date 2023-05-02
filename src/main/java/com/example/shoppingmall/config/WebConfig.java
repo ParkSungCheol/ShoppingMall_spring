d65ec26d52cc45539a1ad042c667e824776c3fbe
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-02 11:22:36 +0900

    test

--- src/main/java/com/example/shoppingmall/config/WebConfig.java
+++ src/main/java/com/example/shoppingmall/config/WebConfig.java
@@ -23,6 +23,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthLoginInterceptor())
                 .addPathPatterns("/*") // 해당 경로에 접근하기 전에 인터셉터가 가로챈다.
-                .excludePathPatterns("/goods", "/login", "/sendEmail", "/checkEmail", "/sendMessage", "/checkMessage", "/signup", "/existCheck"); // 해당 경로는 인터셉터가 가로채지 않는다.
+                .excludePathPatterns("/goods", "/login", "/sendEmail", "/checkEmail", "/sendMessage", "/checkMessage", "/signup", "/existCheck", "/updateUser"); // 해당 경로는 인터셉터가 가로채지 않는다.
     }
 }
