58c0b7118c34f6c26f42fc94880e1df669de200a
psc <skfo8gmlakd@naver.com>
2023-05-14 17:17:38 +0900

    test

--- src/main/java/com/example/shoppingmall/config/WebConfig.java
+++ src/main/java/com/example/shoppingmall/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.shoppingmall.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
