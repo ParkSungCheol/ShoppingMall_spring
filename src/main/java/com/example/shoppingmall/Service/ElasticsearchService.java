46ab5e74235e8aa95a27d1f983d45ac9d2a998a9
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-30 14:15:36 +0900

    test

--- src/main/java/com/example/shoppingmall/Service/ElasticsearchService.java
+++ src/main/java/com/example/shoppingmall/Service/ElasticsearchService.java
@@ -3,22 +3,24 @@ package com.example.shoppingmall.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.StringQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
-
 import com.example.shoppingmall.Domain.Goods;
 import com.example.shoppingmall.Domain.SearchDto;
 
 @Service
 public class ElasticsearchService {
     private final ElasticsearchOperations elasticsearchOperations;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public ElasticsearchService(ElasticsearchOperations elasticsearchOperations) {
@@ -26,20 +28,24 @@ public class ElasticsearchService {
     }
 
     public List<Goods> getDataFromElasticsearch(SearchDto params) {
-    	String queryString = "{\"query\": {"
-    		    + "     \"bool\": {"
-    		    + "       \"should\": ["
-    		    + "          { \"match\": { \"name\": \"틴캐시 5만\" } },"
-    		    + "          { \"match\": { \"name.nori\": \"틴캐시 5만\" } },"
-    		    + "          { \"match\": { \"name.ngram\": \"틴캐시 5만\" } }"
-    		    + "       ]"
-    		    + "     }"
-    		    + "  }}";
+    	// BoolQueryBuilder 생성
+    	BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
 
-        StringQuery stringQuery = new StringQuery(queryString);
-        logger.info("stringQuery : " + stringQuery.toString() );
+    	// MatchQueryBuilder를 사용하여 should 절에 조건 추가
+    	MatchQueryBuilder matchQuery1 = QueryBuilders.matchQuery("name", "틴캐시 5만");
+    	MatchQueryBuilder matchQuery2 = QueryBuilders.matchQuery("name.nori", "틴캐시 5만");
+    	MatchQueryBuilder matchQuery3 = QueryBuilders.matchQuery("name.ngram", "틴캐시 5만");
 
-        SearchHits<Goods> searchHits = elasticsearchOperations.search(stringQuery, Goods.class);
+    	boolQuery.should(matchQuery1);
+    	boolQuery.should(matchQuery2);
+    	boolQuery.should(matchQuery3);
+
+    	// NativeSearchQuery를 사용하여 쿼리 실행
+    	NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+    	        .withQuery(boolQuery)
+    	        .build();
+
+        SearchHits<Goods> searchHits = elasticsearchOperations.search(searchQuery, Goods.class);
         List<Goods> dataList = new ArrayList<>();
         for (SearchHit<Goods> searchHit : searchHits) {
             dataList.add(searchHit.getContent());
