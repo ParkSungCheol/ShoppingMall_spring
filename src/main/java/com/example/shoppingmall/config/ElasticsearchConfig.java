4503ad431c20315ab3ba0989ea80b6edad2fcbb0
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-31 17:35:09 +0900

    test

--- src/main/java/com/example/shoppingmall/config/ElasticsearchConfig.java
+++ src/main/java/com/example/shoppingmall/config/ElasticsearchConfig.java
@@ -1,5 +1,7 @@
 package com.example.shoppingmall.config;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +20,17 @@ public class ElasticsearchConfig {
                 .build();
 
         return new ElasticsearchRestTemplate(RestClients.create(clientConfiguration).rest());
+    }
+    
+    @Bean
+    public RestHighLevelClient client() {
+    	return new RestHighLevelClient(
+    		    RestClient.builder(
+    		        // Elasticsearch 호스트 및 포트 설정
+    		        new HttpHost("ec2-54-180-119-204.ap-northeast-2.compute.amazonaws.com", 3308, "http")
+    		        // 추가적인 Elasticsearch 호스트나 포트가 있다면 여기에 추가
+    		    )
+    		);
     }
 }
 
