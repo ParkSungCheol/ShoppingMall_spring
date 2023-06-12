547097bd20c05407a38704e58f3b8ef06aaa825b
ParkSungCheol <skfo8gmlakd@naver.com>
2023-06-12 11:08:29 +0900

    test

--- src/main/java/com/example/shoppingmall/config/WebConfig.java
+++ src/main/java/com/example/shoppingmall/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ec2-43-200-181-206.ap-northeast-2.compute.amazonaws.com:8080", "http://localhost:8080", "https://www.jurospring.o-r.kr")
+                .allowedOrigins("http://ec2-15-164-222-127.ap-northeast-2.compute.amazonaws.com:8080", "http://localhost:8080", "https://www.jurospring.o-r.kr")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
                 .allowCredentials(true)
                 .exposedHeaders("Token");
