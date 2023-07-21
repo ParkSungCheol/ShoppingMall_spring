638a5c8a24a5100545da987781fa2b8d094ecc7a
ParkSungCheol <skfo8gmlakd@naver.com>
2023-07-21 11:09:53 +0900

    hikariCP 적용

--- src/main/java/com/example/shoppingmall/config/DbConfig.java
+++ src/main/java/com/example/shoppingmall/config/DbConfig.java
@@ -1,25 +1,28 @@
 package com.example.shoppingmall.config;
 
+import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @MapperScan(value = "com.example.shoppingmall", sqlSessionFactoryRef = "SqlSessionFactory")
 public class DbConfig {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
     @Value("${spring.datasource.mapper-locations}")
     String mPath;
 
@@ -28,10 +31,18 @@ public class DbConfig {
     String mybatisConfigPath;
 
 
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
     @Bean(name = "dataSource")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource DataSource() {
-        return DataSourceBuilder.create().build();
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        log.info(dataSource.toString());
+        return dataSource;
     }
 
 
